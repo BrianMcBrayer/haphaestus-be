59c0938782a2ab980587e403080c49bf5c78bd9d
Brian McBrayer <brandmcbrayer@live.com>
2019-02-03 03:59:56 -0500

    Added mock get functionality

---
+++ haphaestusBe/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace haphaestusBe.Models
+{
+    public class Employee
+    {
+        public int Id { get; }
+
+        public PersonName Name { get; }
+
+        public bool IsSpouseEnabled { get; }
+
+        public PersonName Spouse { get; }
+
+        public IReadOnlyList<PersonName> Dependents { get; }
+
+        public Employee(
+            int id,
+            PersonName name,
+            bool isSpouseEnabled,
+            PersonName spouse,
+            IReadOnlyList<PersonName> dependents)
+        {
+            // TODO Guards
+
+            Id = id;
+            Name = name;
+            IsSpouseEnabled = isSpouseEnabled;
+            Spouse = spouse;
+            Dependents = dependents;
+        }
+    }
+}
